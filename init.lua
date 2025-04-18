a0db8413551f74c598eb8ea0b147d06ad448f1c2
Tommy B <118311161+thomas0x2@users.noreply.github.com>
2025-04-18 21:21:22 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<C-q>", vim.cmd.Ex)
+vim.keymap.set("n", "<C-s>", vim.cmd.w)
+vim.wo.number = true
 vim.wo.relativenumber = true
+vim.wo.number = true
+vim.opt.cmdheight = 0
 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
